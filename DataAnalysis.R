180448e391a594e89312415c11b8c98ec61d183f
Stella Lee <34009124+stellasylee@users.noreply.github.com>
2019-10-28 20:54:51 -0500

    add final model and residual plots

--- DataAnalysis.R
+++ DataAnalysis.R
@@ -4,6 +4,7 @@ library(dplyr)
 library(ggplot2)
 library(mosaic)
 library(stringr)
+require(leaps)
 
 # Load final dataset
 HPIdata <- read.csv("https://raw.githubusercontent.com/stellasylee/Happiness-Planet-Index/master/finalDataset.csv")[-1]
@@ -14,6 +15,10 @@ HPIdata$PriHealth <- as.numeric(as.character(HPIdata$PriHealth))
 HPIdata$PPP <- as.numeric(as.character(HPIdata$PPP))
 HPIdata$PopDensity <- as.numeric(as.character(HPIdata$PopDensity))
 HPIdata$WomenParliamant <- as.numeric(as.character(HPIdata$WomenParliamant))
+
+# Happiness Score Distribution
+ggplot(data = HPIdata, aes(x = HPI, y = HPIRank)) + geom_point()  +
+  aes(colour = RegionNum) + theme(legend.position = "right") + labs(title = "")
 
 # Main effect without manipulation
 ggplot(data = HPIdata, aes(x = Internet, y = HPI)) + geom_point()  +
@@ -37,31 +42,28 @@ ggplot(data = HPIdata, aes(x = FreedomToChoice, y = HPI)) + geom_point()  +
 plot(HPIdata$PopDensity)
 plot(log(HPIdata$PopDensity))
 
-
-# Full model
+# Full model ----
 model <- lm (HPI ~ factor(Region) + factor(Year) + Internet + LifeExpect + GovHealth + PriHealth + PPP +
                PopDensity + UnEmployment + FreedomToChoice + Corruption,
                   data = HPIdata)
-anova(model)
 # plot(model)
 
-model_transform <- lm (HPI ~ factor(Region) + factor(Year) + Internet + LifeExpect + GovHealth + PriHealth + PPP +
-               log(PopDensity) + UnEmployment + FreedomToChoice + Corruption,
-             data = HPIdata)
-anova(model_transform)
-
-#Best subsets 
-
-require(leaps)
-full_data=cbind(factor(HPIdata$Region), factor(HPIdata$Year), HPIdata$Internet, HPIdata$LifeExpect, HPIdata$GovHealth, HPIdata$PriHealth, HPIdata$PPP, log(HPIdata$PopDensity),HPIdata$UnEmployment, HPIdata$FreedomToChoice, HPIdata$Corruption)
-
-row.has.na1 <- apply(HPIdata, 1, function(x){any(is.na(x))})
-
+# Best subsets 
+full_data=cbind(factor(HPIdata$Region), factor(HPIdata$Year), HPIdata$Internet, HPIdata$LifeExpect,
+                HPIdata$GovHealth, HPIdata$PriHealth, HPIdata$PPP, log(HPIdata$PopDensity),HPIdata$UnEmployment,
+                HPIdata$FreedomToChoice, HPIdata$Corruption)
+row.has.na <- apply(HPIdata, 1, function(x){any(is.na(x))})
 HPIdata.filtered <- HPIdata[!row.has.na,]
-
 best.lm <- leaps(HPI.data.filtered,HPIdata.filtered$HPI, method = "adjr2", names = c('Region','Year','Internet','Life Expectancy','Gov Health','PriHealth','PPP','Log PopDensity','Unemployment','Freedom to Choice','Corruption'), nbest=3)
-
 data1 = cbind(best.lm$which,best.lm$adjr2)
-View(data1)
+
+# Best subset model -- Final model
+model_transform <- lm (HPI ~ factor(Region) + Internet + LifeExpect + GovHealth  + log(PPP) +
+                         log(PopDensity) + UnEmployment,
+                       data = HPIdata.filtered)
+plot(model_transform)
+
+res <- model_transform$residuals
+plot(log(HPIdata.filtered$PPP), res)
 
 # Extra Sum of Squares for WomenParliamant and LaborForceFemale
