2e9e8e2099e69d5178061675b86384f260505121
Stella Lee <34009124+stellasylee@users.noreply.github.com>
2019-10-28 20:23:42 -0500

    add main effect graph

--- DataAnalysis.R
+++ DataAnalysis.R
@@ -15,21 +15,39 @@ HPIdata$PPP <- as.numeric(as.character(HPIdata$PPP))
 HPIdata$PopDensity <- as.numeric(as.character(HPIdata$PopDensity))
 HPIdata$WomenParliamant <- as.numeric(as.character(HPIdata$WomenParliamant))
 
-# Happiness Score Distribution
-ggplot(data = HPIdata, aes(x = HPI, y = HPIRank)) + geom_point()  +
-  aes(colour = Region) + theme(legend.position = "right") + labs(title = "")
+# Main effect without manipulation
+ggplot(data = HPIdata, aes(x = Internet, y = HPI)) + geom_point()  +
+  aes(colour = factor(RegionNum)) + theme(legend.position = "right") + labs(title = "Main Effect of Internet")
+ggplot(data = HPIdata, aes(x = LifeExpect, y = HPI)) + geom_point()  +
+  aes(colour = factor(RegionNum)) + theme(legend.position = "right") + labs(title = "Main Effect of Life Expectency (yr)")
+ggplot(data = HPIdata, aes(x = GovHealth, y = HPI)) + geom_point()  +
+  aes(colour = factor(RegionNum)) + theme(legend.position = "right") + labs(title = "Main Effect of Government Health Expenditure")
+ggplot(data = HPIdata, aes(x = PriHealth, y = HPI)) + geom_point()  +
+  aes(colour = factor(RegionNum)) + theme(legend.position = "right") + labs(title = "Main Effect of Private Health Expenditure")
+ggplot(data = HPIdata, aes(x = PPP, y = HPI)) + geom_point()  +
+  aes(colour = factor(RegionNum)) + theme(legend.position = "right") + labs(title = "Main Effect of Purchasing Power")
+ggplot(data = HPIdata, aes(x = PopDensity, y = HPI)) + geom_point()  +
+  aes(colour = factor(RegionNum)) + theme(legend.position = "right") + labs(title = "Main Effect of Population Density")
+ggplot(data = HPIdata, aes(x = UnEmployment, y = HPI)) + geom_point()  +
+  aes(colour = factor(RegionNum)) + theme(legend.position = "right") + labs(title = "Main Effect of Unemployment")
+ggplot(data = HPIdata, aes(x = FreedomToChoice, y = HPI)) + geom_point()  +
+  aes(colour = factor(RegionNum)) + theme(legend.position = "right") + labs(title = "Main Effect of Freedom to Choice")
+
+## According to main effect plots, we can see that PopDensity is very skewed with extreme values
+plot(HPIdata$PopDensity)
+plot(log(HPIdata$PopDensity))
+
 
 # Full model
 model <- lm (HPI ~ factor(Region) + factor(Year) + Internet + LifeExpect + GovHealth + PriHealth + PPP +
                PopDensity + UnEmployment + FreedomToChoice + Corruption,
                   data = HPIdata)
 anova(model)
-res <- model$residuals
-plot(res)
-qqnorm(res)
-mplot(HPIdata)
+# plot(model)
 
-HPI$Region <- as.character(HPI$Region)
-group1 <- filter(HPI, Region[1] == '1')
+model_transform <- lm (HPI ~ factor(Region) + factor(Year) + Internet + LifeExpect + GovHealth + PriHealth + PPP +
+               log(PopDensity) + UnEmployment + FreedomToChoice + Corruption,
+             data = HPIdata)
+anova(model_transform)
 
 # Extra Sum of Squares for WomenParliamant and LaborForceFemale
